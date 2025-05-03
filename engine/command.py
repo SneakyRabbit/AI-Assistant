4582d5631e5ea0a21f2de8f2511af3ca2c4a07fa
SneakyRabbit <asdfghjkl.nl3251@gmail.com>
2025-05-03 18:15:55 +0545

    Speech Recognition

--- engine/command.py
+++ engine/command.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr
 
 
 def speak(text):
@@ -10,4 +11,25 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-speak("Hello, Sir")
+def takecommand():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print('Recognizing')
+        query = r.recognize_vosk(audio, language='en')
+        print(f"User said: {query}")
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+text = takecommand()
+
+speak(text)
